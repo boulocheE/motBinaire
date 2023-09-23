2aa759b44a8eac234464b05549cc3304e5a144a9
blc_ele <boulocheeleonore28@gmail.com>
2023-09-23 18:16:00 +0200

    written methods

--- MotBinaire.java
+++ MotBinaire.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 
+import java.lang.Math;
+
 
 public class MotBinaire
 {
@@ -113,5 +115,40 @@ public class MotBinaire
 
 
 	public String toString () { return Arrays.toString(this.mot); }
+
+	// public boolean equals(Object other)
+	// {
+	// 	if ( other == null ) return false;
+
+	// 	if ( other.nbBits() != this.nbBits() ) return false;
+
+	// 	return true;
+	// }
+
+	public static MotBinaire[] genererMots(int n)
+	{
+		MotBinaire[] tab;
+		int[][] tabListeMots;
+		int bin = 0;
+		int res = 0;
+
+		tab          = new MotBinaire[(int) (Math.pow(n, n))];
+		tabListeMots = new int       [(int) (Math.pow(n, n))][n];
+
+		for ( int cpt1 = 0; cpt1 < tabListeMots.length; cpt1 ++ )
+		{
+			for ( int cpt2 = n-1; cpt2 <= 0; cpt2 ++ )
+			{
+				tabListeMots[cpt1][cpt2] = res%2;
+				res = tabListeMots[cpt1][cpt2];
+			}
+
+			res = ++bin;
+		}
+
+		for ( int i = 0; i < tab.length; i ++ ) tab[i] = MotBinaire.fabrique(tabListeMots[i]);
+
+		return tab;
+	}
 
 }
